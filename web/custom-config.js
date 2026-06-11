7f64f67b8bbf2e2d2859c12fe6994c5fac5f6bc9
Ciro De Vita <ciro.devita@corabea.it>
2026-06-11 15:05:12 +0000

    Added new server for jvb balancing

--- web/custom-config.js
+++ web/custom-config.js
@@ -1,1 +1,10 @@
 config.defaultLogoUrl = 'https://meet.corabea.it/images/corabea-logo.png';
+
+config.prejoinConfig = {
+    enabled: true,
+    hideDisplayName: false
+};
+
+config.bridgeChannel = {
+    preferSctp: false
+};
