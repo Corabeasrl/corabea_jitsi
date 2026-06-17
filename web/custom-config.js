fa58229e8f559c6803808aca7acf777eafc9a133
Ciro De Vita <ciro.devita@corabea.it>
2026-06-17 19:34:04 +0000

    Updated web config

--- web/custom-config.js
+++ web/custom-config.js
@@ -9,16 +9,10 @@ config.bridgeChannel = {
     preferSctp: false
 };
 
-config.resolution = 1080;
-config.constraints = {
-    video: {
-        height: { ideal: 1080, max: 1080, min: 180 },
-        width:  { ideal: 1920, max: 1920, min: 320 },
-    }
-};
-
 config.videoQuality = config.videoQuality || {};
-config.videoQuality.codecPreferenceOrder = ["VP8", "VP9", "H264", "AV1"];
-config.videoQuality.mobileCodecPreferenceOrder = ["VP8", "H264", "VP9", "AV1"];
-config.p2p = config.p2p || {};
-config.p2p.codecPreferenceOrder = ["VP8", "H264", "VP9", "AV1"];
+config.videoQuality.maxBitratesVideo = {
+    low: 300000,
+    standard: 800000,
+    high: 2500000,
+    fullHd: 4000000
+};
