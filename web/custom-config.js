9ff41f175e208b5024a1ff249a2d535ec87933b9
Ciro De Vita <ciro.devita@corabea.it>
2026-06-26 20:30:42 +0000

    Disable P2P on 1:1 calls, switch to VP8/Opus RED for stable quality

--- web/custom-config.js
+++ web/custom-config.js
@@ -9,10 +9,18 @@ config.bridgeChannel = {
     preferSctp: false
 };
 
+config.enableOpusRed = true;
+
+config.p2p = config.p2p || {};
+config.p2p.enabled = false;
+config.p2p.stunServers = [
+    { urls: 'stun:meet.corabea.it:3478' }
+];
+
 config.videoQuality = config.videoQuality || {};
 config.videoQuality.maxBitratesVideo = {
-    low: 300000,
-    standard: 800000,
-    high: 2500000,
-    fullHd: 4000000
+    VP8:  { low: 200000, standard: 800000, high: 2500000, fullHd: 4000000, ultraHd: 6000000 },
+    VP9:  { low: 100000, standard: 600000, high: 2000000, fullHd: 3500000, ultraHd: 5000000 },
+    AV1:  { low: 100000, standard: 500000, high: 1500000, fullHd: 3000000, ultraHd: 5000000 },
+    H264: { low: 200000, standard: 800000, high: 2500000, fullHd: 4000000, ultraHd: 6000000 }
 };
